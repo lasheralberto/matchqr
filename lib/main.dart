6d91f7766fd147f539ef66a2bdbe1950ee7e46b1
Alberto Martinez Lasheras <72318269+lasheralberto@users.noreply.github.com>
2024-02-10 15:07:31 +0100

    mm

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'package:payment_tool/cuadraditosLanding.dart';
 import 'package:payment_tool/initialLanding.dart';
 import 'firebase_options.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 
 GoogleSignIn googleSignIn = GoogleSignIn(
